e2ba06992a94502babdcf00491f6d4542eefed3f
Marcin Dlubis <marcindl@hotmail.com>
2023-05-28 11:14:11 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './components/one/one.component';
+import { TwoComponent } from './components/two/two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: 'one', pathMatch: "full"},
+  {path:"one", component: OneComponent},
+  {path:"two", component: TwoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
